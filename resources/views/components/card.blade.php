a0dbb473232731105105907d9960dc7278a57bec
Ayush Niraula <ayushniraula@Ayushs-MacBook-Air.local>
2025-01-14 19:51:35 +0545

    Initial commit for Laravel Herd project

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,22 @@
+    <a href="#" class="flex flex-col gap-4 rounded-md border border-solid border-gray-300 px-4 py-8 md:p-0">
+        <img src="https://images.pexels.com/photos/1629212/pexels-photo-1629212.jpeg?cs=srgb&dl=pexels-minan1398-1629212.jpg&fm=jpg"
+            alt="" class="h-60 object-cover" />
+        <div class="px-6 py-4">
+            <p class="mb-4 text-sm font-semibold uppercase text-gray-500"> lifestyle </p>
+            <p class="mb-4 text-xl font-semibold"> The latest news with Flowspark </p>
+            <p class="mb-6 text-sm text-gray-500 sm:text-base lg:mb-8"> Lorem ipsum dolor sit amet, &nbsp;elit ut
+                aliquam, purus sit amet luctus venenatis elit ut aliquam, purus sit amet luctus venenatis </p>
+            <div class="flex">
+                <img src="https://images.pexels.com/photos/1629212/pexels-photo-1629212.jpeg?cs=srgb&dl=pexels-minan1398-1629212.jpg&fm=jpg"
+                    alt="" class="mr-4 h-10 w-10 rounded-full object-cover" />
+                <div class="flex flex-col">
+                    <h6 class="text-base font-bold">Laila Bahar</h6>
+                    <div class="flex flex-col lg:flex-row">
+                        <p class="text-sm text-gray-500">Sept 28, 2022</p>
+                        <p class="mx-2 hidden text-sm text-gray-500 lg:flex"> - </p>
+                        <p class="text-sm text-gray-500">6 mins read</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <section>
+        <!-- Container -->
+        <div class="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
+            <!-- Component -->
+            <div class="flex flex-col items-center">
+                <h2 class="text-center text-3xl font-bold md:text-5xl"> The latest and greatest news </h2>
+                <p class="mb-8 mt-4 text-center text-sm text-gray-500 sm:text-base md:mb-12 lg:mb-16"> Lorem ipsum dolor
+                    sit amet elit ut aliquam </p>
+                <!-- Content -->
+                <div
+                    class="mb-6 grid gap-4 sm:grid-cols-2 sm:justify-items-stretch md:mb-10 md:grid-cols-3 lg:mb-12 lg:gap-6">
+
+
+                    <x-card />
+
+
+                </div>
+                <!-- Button -->
+                <a href="#" class="rounded-md bg-black px-6 py-3 text-center font-semibold text-white"> Create Note
+                </a>
+            </div>
+        </div>
+    </section>
+</x-layout>
